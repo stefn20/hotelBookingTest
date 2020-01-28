a87bc48bb7731cba81fbd26cc209ede37edcf8c7
stefan <stefan@n20technologies.com>
2020-01-28 00:11:15 +0000

    Finish book scenario to assert booking is successfully saved and displayed

--- src/test/java/businesstest/pages/BookingsPage.java
+++ src/test/java/businesstest/pages/BookingsPage.java
@@ -1,9 +1,14 @@
 package businesstest.pages;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @DefaultUrl("/")
 public class BookingsPage extends PageObject {
@@ -16,6 +21,8 @@ public class BookingsPage extends PageObject {
     private WebElementFacade checkout;
     @FindBy(xpath = "//input[@type='button' and @value=' Save ']")
     private WebElementFacade saveButton;
+    @FindBy(id = "bookings")
+    private WebElementFacade bookingsTable;
 
     public void addBooking(String firstName, String surname, String price, boolean deposit, String checkInDate, String checkOutDate) {
         typeInto(firstname, firstName);
@@ -25,5 +32,10 @@ public class BookingsPage extends PageObject {
         typeInto(checkin, checkInDate);
         typeInto(checkout, checkOutDate);
         saveButton.click();
+    }
+
+    public List<String> retrieveBookings() {
+        List<WebElement> retrievedBookings = bookingsTable.findElements(By.cssSelector("div.row[id]"));
+        return retrievedBookings.stream().map(WebElement::getText).collect(Collectors.toList());
     }
 }
--- src/test/java/businesstest/steps/BookingStepDefinitions.java
+++ src/test/java/businesstest/steps/BookingStepDefinitions.java
@@ -3,6 +3,12 @@ package businesstest.steps;
 import businesstest.model.Booking;
 import businesstest.pages.BookingsPage;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import net.serenitybdd.core.Serenity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookingStepDefinitions {
 
@@ -11,6 +17,7 @@ public class BookingStepDefinitions {
     @Given("^user adds a new booking")
     public void userAddsANewBooking() {
         Booking booking = Booking.withRandomPrice();
+        Serenity.setSessionVariable("booking").to(booking);
 
         bookingsPage.open();
         bookingsPage.addBooking(
@@ -21,5 +28,16 @@ public class BookingStepDefinitions {
                 booking.checkInDate.toString(),
                 booking.checkOutDate.toString()
         );
+    }
+
+    @Then("^the booking is displayed in the bookings list")
+    public void theBookingIsDisplayedInTheBookingsList() throws InterruptedException {
+        Booking booking = Serenity.sessionVariableCalled("booking");
+        String bookingDetails = booking.firstName + "\n" + booking.surname + "\n" + booking.price + "\n" + booking.depositPaid + "\n" + booking.checkInDate + "\n" + booking.checkOutDate;
+
+        bookingsPage.open();
+        Thread.sleep(1000); // Horrible but not entirely sure what element on the page to wait for to avoid having to Thread.sleep()
+        List<String> retrievedBookingDetails = bookingsPage.retrieveBookings();
+        assertThat(retrievedBookingDetails.stream().anyMatch(b -> b.equals(bookingDetails))).isTrue();
     }
 }
